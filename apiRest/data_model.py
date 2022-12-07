046a6778f6706c27b948d07d18de280c55b07cbb
j2sanabriam <jj.sanabria@uniandes.edu.co>
2022-12-06 23:52:13 -0500

    ajuste

--- apiRest/data_model.py
+++ apiRest/data_model.py
@@ -19,23 +19,3 @@ class DataModel(BaseModel):
     PaperlessBilling: str
     PaymentMethod: str
 
-"""
-    Tasa_de_Interes: float
-    Plazo_OK: str
-    Cliente: str
-    Producto_Destinacion_OK: str
-    Canal_de_Captura_OK: str
-    Tipo_de_Empresa: str
-    Ano_de_Constitucion: int
-    Actividad_eco_SEMPLI: str
-    Ciudad_agrupada: str
-    Estrato: int
-    Acceso_a_la_banca: str
-    Mujeres_Empresarias: str
-    Tipo_de_Garantia: str
-
-    features = ['SeniorCitizen', 'TotalCharges', 'gender', 'Partner', 'Dependents', 'PhoneService', 'MultipleLines',
-                'InternetService', 'OnlineSecurity', 'OnlineBackup',
-                'DeviceProtection', 'TechSupport', 'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
-                'PaymentMethod']
-"""
